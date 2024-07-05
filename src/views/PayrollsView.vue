4f49a3baff5bb8d1db5d170c50ffa59d8677a299
Mel Escolano <amalia.escolano@gmail.com>
2024-07-05 14:16:24 +0200

    Created payrolls view

---
+++ src/views/PayrollsView.vue
@@ -0,0 +1,20 @@
+<template>
+  <basic-layout>
+    <h1>Estamos en Payrolls</h1>
+    <p>hola mundo</p>
+    <button>cerrar</button>
+  </basic-layout>
+</template>
+
+<script>
+import BasicLayout from "../layouts/BasicLayout.vue";
+
+export default {
+  name: "Payrolls",
+  components: {
+    BasicLayout,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
