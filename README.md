d73ae6ef054fe7b301d859203b402472e453b3bc
Mel Escolano <amalia.escolano@gmail.com>
2024-07-05 14:11:04 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,19 +1,25 @@
 # vue-payroll-manager
 
+Proyecto de prueba hecho con vue, usando firebase para la autenticacion de usuarios.
+
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const webpack = require("webpack"); //To avoid the warning
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+
+  // start To avoid the warning "runtime-core.esm-bundler.js:4060 Feature flag __VUE_PROD_HYDRATION_MISMATCH_DETAILS__ is not explicitly defined. You are running the esm-bundler build of Vue, which expects these compile-time feature flags to be globally injected via the bundler config in order to get better tree-shaking in the production bundle."
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: true, // habilitar/deshabilitar API de opciones
+        __VUE_PROD_DEVTOOLS__: false, // habilitar/deshabilitar herramientas de desarrollo en producción
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false, // define esta bandera explícitamente
+      }),
+    ],
+  },
+  //end to avoid warning
+});
